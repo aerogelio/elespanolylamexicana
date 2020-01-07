84c4ff4ced5d4aa0af5a9e5796b30b040cab1e28
Rogelio Amaya <rogelioamayaesp@outlook.com>
2020-01-07 00:05:28 -0700

    Add connections settings DB

--- elespanolylamexicana/settings.py
+++ elespanolylamexicana/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'elespanolylamexicana.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    """
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }"""
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fuck_db',
+        'USER': 'fuck_u',
+        'PASSWORD': 'fuck_p',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
