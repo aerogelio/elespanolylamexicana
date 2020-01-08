68087cfdf9c4725d62de89fbeebee699c1ef131e
Rogelio Amaya <Rogelioamayaesp@outlook.com>
2020-01-08 09:11:28 +0000

    Try fix settings

--- elespanolylamexicana/settings.py
+++ elespanolylamexicana/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%8+!jac2o!pki1ue3r8fo27t#3zgmtw(b)1g(pf1jq*6l(urk3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
